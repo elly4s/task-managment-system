eb120ca867ef2a6d95f1abfc9d3d247a1094d3eb
Elly <elly4sionov@gmail.com>
2024-03-03 20:33:19 +0200

    inital commit

---
+++ src/models/Task.ts
@@ -0,0 +1,40 @@
+enum TaskStatus {
+    TODO = "TODO",
+    IN_PROGRESS = "IN_PROGRESS",
+    COMPLETE = "COMPLETE"
+}
+
+class Task {
+    private id: number
+    private title: string
+    private description?: string
+    private dueDate?: Date
+    private status: TaskStatus
+
+    constructor(id: number, title: string, status: TaskStatus, description?: string, dueDate?: Date) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.status = status;
+    }
+
+    updateStatus(status: TaskStatus): void {
+        this.status = status;
+    }
+
+    setDueDate(dueDate: Date): void {
+        this.dueDate = dueDate;
+    }
+
+    displayTaskDetails(): void {
+        console.log("Id of task: ", this.id);
+        console.log("Title of task: ", this.title);
+        if (this.description) console.log("Description of task: ", this.description);
+        if (this.dueDate) console.log("Due Date of task: ", this.dueDate);
+        console.log("Status of task: ", this.status);
+    }
+    
+}
+
+export default Task;
---
+++ src/models/User.ts
@@ -0,0 +1,9 @@
+import Task from './Task.ts'
+
+
+class User {
+    private id: number;
+    private email: string;
+    private name: string;
+    private tasks: Task[];
+}
